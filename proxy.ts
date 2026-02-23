1e78197f715378cfbb0f1fc9ac5e60e05c096286
Ryann Pastolero <ryannapastolero@gmail.com>
2026-02-23 17:29:06 -0600

    Pushed working method to get session data from backend

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,20 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
  
-export function proxy(request: NextRequest) {
-  
+export async function proxy(request: NextRequest) {
+
+  const session = request.cookies.get('session')
+  var data = ''
+
+  if (session) {
+    const fetched = await fetch('http://localhost:8000/auth/session', {
+      headers: {
+        cookie : `session=${session.value}`
+      }
+    })
+
+    data = await fetched.json()
+  }
+  return NextResponse.json({sessionData : data})
 }
   
