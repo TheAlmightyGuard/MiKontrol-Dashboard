5238d57a9f3c0c6b351ca4e1d63c029ba222cd78
Ryann Pastolero <ryannapastolero@gmail.com>
2026-03-24 19:54:15 -0500

    Fixed missing variable in prop

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,10 +1,19 @@
-import Navbar from '@/app/lib/components/Navbar'
+"use server"
 
-export default function About() {
+import Navbar from '@/app/lib/components/Navbar'
+import Footer from '@/app/lib/components/Footer';
+import { getUser } from "@/app/lib/functions/cookies";
+
+export default async function Page() {
+
+  const userData = await getUser()
 
   return (
-    <div className="background">
-      <Navbar/>
+    <div className='background'>
+        <header>
+            <Navbar data={userData}/>
+        </header>
+        <Footer/>
     </div>
   );
 }
